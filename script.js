ffe19ad7343088fe2e37abca018893b0c9200569
LoicBrassart <loic.brassart@gmail.com>
2020-09-28 00:52:15 +0200

    Added everything needed for basic calculus. Need testing git add .

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-// Insert awesome game here!
-const userName = prompt("What's your name ?");
-const randNumber = Math.floor(Math.random() * 100) + 1;
-let userTry = null;
-const initialTries = 10;
-let remainingTries = initialTries;
 
-while (randNumber !== userTry && remainingTries > 0) {
-userTry = parseInt(prompt("What's your guess ? (1~100)"));
-remainingTries--;
-if (userTry > randNumber) {
-    console.log("That's less");
-} else if (userTry < randNumber) {
-    console.log("That's more");
+function cal(ope1, operator, ope2) {
+    ope1=parseInt(ope1);
+    ope2=parseInt(ope2);
+    
+    switch(operator){
+        case "+":
+            return ope1+ope2;
+        case "-":
+            return ope1-ope2;
+        case "*":
+            return ope1*ope2;
+        case "/":
+            if(ope2!==0)
+                return ope1/ope2;
+            return "Invalid operand ! CANNOT DIVIDE BY 0 !!!"
+        default:
+            return "Invalid operator!"
+    }
 }
-}
-if (randNumber === userTry) {
-console.log("You win! " + userName + ", the winner is you !");
-console.log(`(You used ${initialTries - remainingTries} tries.)`);
-} else {
-console.log("BOOO! YOU LOSE !");
-}
+
+const formCalc = document.getElementById("form1");
+const ope1=document.getElementById("val1");
+const ope2=document.getElementById("val2");
+const operator=document.getElementById("ope");
+const resSpan = document.getElementById("res");
+
+formCalc.addEventListener("submit", function() {
+    const res = cal(ope1, operator, ope2);
+    resSpan.innerHTML=res;
+})
+
+
